708d10d5bc06cdc3141607001ef847d6a182855c
Logan Jahnke <ljahnke9@gmail.com>
2022-05-28 17:31:46 -0500

    responsive drawer updates

--- src/Assets/Components/Layout/AHDrawer.tsx
+++ src/Assets/Components/Layout/AHDrawer.tsx
@@ -113,8 +113,9 @@ export function AHDrawer(props: AHDrawerProps)
 				<Button className="ahTab" onClick={goHome}>SpartanRecord.com</Button>
 			</Toolbar>
 			<Divider flexItem />
+			<Box sx={{ pt: 5, pb: 5, overflowY: "auto" }}>
 				{player && player.gamertag && isAllowed ? 
-				<Tabs orientation="vertical" value={currentTab || "Search"} onChange={tabClicked} sx={{ mt: 5 }}>
+				<Tabs orientation="vertical" value={currentTab || "Search"} onChange={tabClicked}>
 					<Tab className="ahTab" value="Search" label="Search" icon={<PersonSearchIcon />} iconPosition="start" />
 					<Tab className="ahTab" value="Service Record" label="Service Record" icon={<ModeStandbyIcon />} iconPosition="start" />
 					<Tab className="ahTab" value="Playlists" label="Playlists" icon={<ListIcon fontSize="small" />} sx={{ fontSize: "0.75rem", ml: 3, minHeight: 0 }} iconPosition="start" />
@@ -131,7 +132,7 @@ export function AHDrawer(props: AHDrawerProps)
 					{/* <Tab className="ahTab" value="Company" label="Spartan Company" icon={<GroupsIcon />} iconPosition="start" /> */}
 				</Tabs>
 				: player && player.gamertag ?
-				<Tabs orientation="vertical" value={currentTab || "Search"} onChange={tabClicked} sx={{ mt: 5 }}>
+				<Tabs orientation="vertical" value={currentTab || "Search"} onChange={tabClicked}>
 					<Tab className="ahTab" value="Search" label="Search" icon={<PersonSearchIcon />} iconPosition="start" />
 					<Tab className="ahTab" value="Service Record" label="Service Record" icon={<ModeStandbyIcon />} iconPosition="start" />
 					<Tab className="ahTab" value="Playlists" label="Playlists" icon={<ListIcon fontSize="small" />} sx={{ fontSize: "0.75rem", ml: 3, minHeight: 0 }} iconPosition="start" />
@@ -148,11 +149,12 @@ export function AHDrawer(props: AHDrawerProps)
 					{/* <Tab className="ahTab" value="Company" label="Spartan Company" icon={<GroupsIcon />} iconPosition="start" /> */}
 				</Tabs>
 				:
-				<Tabs orientation="vertical" value={currentTab || "Search"} onChange={tabClicked} sx={{ mt: 5 }}>
+				<Tabs orientation="vertical" value={currentTab || "Search"} onChange={tabClicked}>
 					<Tab className="ahTab" value="Search" label="Search" icon={<PersonSearchIcon />} iconPosition="start" />
 					{/* <Tab className="ahTab" value="Company" label="Spartan Company" icon={<GroupsIcon />} iconPosition="start" /> */}
 				</Tabs>
 				}
+			</Box>
 			<Divider flexItem sx={{ flexGrow: 1 }} />
 			<Box sx={{ display: "flex", flexDirection: "column", p: 1 }}>
 				<Typography variant="subtitle2"><Link href="https://www.paypal.com/donate/?hosted_button_id=FTQWP27P5JMPG">Donate with PayPal</Link></Typography>
