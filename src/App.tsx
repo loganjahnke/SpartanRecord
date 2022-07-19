307f54d14284ab463e847e58e11f551e4488f1f9
Logan Jahnke <ljahnke9@gmail.com>
2022-07-18 19:25:02 -0500

    remove production flag override

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ import { LeaderboardView } from "./Pages/LeaderboardView";
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	databaseURL: process.env.NODE_ENV === "production" || true ? process.env.REACT_APP_PROD_URL : process.env.REACT_APP_TEST_URL,
+	databaseURL: process.env.NODE_ENV === "production" ? process.env.REACT_APP_PROD_URL : process.env.REACT_APP_TEST_URL,
 	projectId: process.env.REACT_APP_PROJECT_ID,
 	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
 	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
